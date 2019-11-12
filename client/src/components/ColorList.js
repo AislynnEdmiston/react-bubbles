c955dbc7cfac5ed671ec846fd0e4ceba1ceecf89
AislynnEdmiston <davidandaislynn@gmail.com>
2019-11-11 22:00:36 -0500

    MVP

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ colors, updateColors, refresh }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
@@ -21,10 +23,27 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+
+    axiosWithAuth().put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(refresh())
+      .then(res => {
+        console.log(res.data)
+        setEditing(false)
+      })
+      .catch(err => console.log(err))
+
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth().delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(refresh())
+      .then(res => {
+        console.log('Color was Deleted', res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   };
 
   return (
